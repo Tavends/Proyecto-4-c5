0449defc17b9713e4ee927f02684b6057db4959a
tavends <toavendano.dwfsc5@bootcampudd.cl>
2023-07-05 16:36:42 -0400

    cambios en css

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,6 +1,22 @@
 export function AboutUs() {
   return (
-    <h1>Ven a probar nuestros exquisitos ramen, costillas y Alitas</h1>,
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE-REV8VQaAiItlRR6IBRkBuXrBiw-uJD-oM8QQiutMPZe4qEwtQPGApCQTMUGwIzmxqI&usqp=CAU" alt="" />
+    <div className="portada">
+      <header className="portada__header">
+        <h1 className="portada__titulo">Bienvenidos a [AWR Food SpA]</h1>
+        <p className="portada__descripcion">¡Bienvenidos a "AWR Food"! Tu destino culinario para una experiencia auténtica de ramen, alitas de pollo y costillas BBQ. Sumérgete en nuestro mundo de deliciosos sabores y descubre una gama diversa de opciones que te harán agua la boca.
+
+Ramen, el plato icónico de la cocina japonesa, es nuestra especialidad principal. Nuestros chefs expertos preparan con esmero este plato de fideos de origen oriental, sumergidos en caldos exquisitamente sazonados y cargados de ingredientes frescos. Desde los clásicos como el tonkotsu y el shoyu, hasta creaciones innovadoras que fusionan sabores de todo el mundo, nuestra selección de ramen te sorprenderá y deleitará.
+
+Si eres amante de las alitas de pollo, estás en el lugar adecuado. Nuestro menú ofrece alitas crujientes y jugosas, disponibles en tres estilos diferentes: fritas, asadas y horneadas. Pero eso no es todo, ¡nuestras salsas son la estrella del espectáculo! Desde opciones suaves y llenas de sabor hasta sabores picantes que desafiarán tu paladar, nuestras salsas artesanales complementarán perfectamente tus alitas de pollo, brindándote una experiencia gastronómica única.
+
+Y para aquellos que buscan deleitarse con costillitas BBQ, no se sentirán decepcionados. Nuestro chef especializado en parrillas domina el arte de las costillas tiernas y jugosas, cocinadas a la perfección y bañadas en nuestra irresistible salsa BBQ casera. Si prefieres un sabor suave y ahumado o un toque picante que te haga desafiar tus límites, tenemos una variedad de opciones para satisfacer todos los gustos.
+
+En "AWR Food" nos enorgullece ofrecer un ambiente acogedor y un servicio excepcional. Nuestro personal amable y atento estará encantado de ayudarte a seleccionar el plato perfecto y brindarte recomendaciones basadas en tus preferencias personales.
+
+¡Ven a nuestro restaurante y déjanos llevar tus papilas gustativas a un viaje inolvidable! Descubre los auténticos sabores del ramen, alitas de pollo y costillas BBQ en "AWR Food" y déjate seducir por nuestras múltiples opciones de salsas, desde las más suaves hasta las más picantes. ¡Te garantizamos una experiencia culinaria que no olvidarás!</p>
+      </header>
+    </div>
   )
 }
+
+export default AboutUs;
